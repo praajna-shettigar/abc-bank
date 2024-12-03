83afb18ecdd34b91592920d7830e9391bc060bef
praajna-shettigar <56425316+praajna-shettigar@users.noreply.github.com>
2024-12-03 11:07:51 +0530

    Unit test covering Transaction class

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -10,4 +10,21 @@ public class TransactionTest {
         Transaction t = new Transaction(5);
         assertTrue(t instanceof Transaction);
     }
+
+    @Test
+    public void testTransactionAmount(){
+        double amount = 100.0;
+        Transcation transaction = new Transaction(amount);
+        assertEquals(amount,transaction.amount,0.0);
+    }
+
+    @Test
+    public void testTransactionDate(){
+        Transaction transaction = new Transaction(100.0);
+        Date now = DateProvider.getInstance().now();
+
+        //samll time difference between instance creation and test run
+        assertTrue(transaction.transactionDate.getTime()<= now.getTime());
+
+    }
 }
